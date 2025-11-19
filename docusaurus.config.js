590b5b01d0cf80ef1a3ae7e812288914463599a1
Alejandro Garrido <alegarrei@alu.edu.gva.es>
2025-11-19 23:06:46 +0100

    Fix enlaces rotos y preparacion para despliegue

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,15 +20,15 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://agarrido04.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docusaurusAlejandro',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'agarrido04', // Usually your GitHub org/user name.
+  projectName: 'docusaurusAlejandro', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
@@ -111,7 +111,7 @@ const config = {
             items: [
               {
                 label: 'Tutorial',
-                to: '/docs/intro',
+                to: '/docs/inici',
               },
             ],
           },
